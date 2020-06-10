0f23207a9f09f21b93703d297c0989ae104fe726
Samantha Banchik <sam11b@berkeley.edu>
2020-06-09 19:11:51 -0700

    Updates

---
+++ precache-manifest.31dea516cd74ac08beb1739764e2b679.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648f10815a2ce3a3e934ed450cdb4afe",
+    "url": "/updated_portfolio/index.html"
+  },
+  {
+    "revision": "a22851a63ea7b960ae49",
+    "url": "/updated_portfolio/static/css/main.9313adcb.chunk.css"
+  },
+  {
+    "revision": "41cb9081b91afa18a685",
+    "url": "/updated_portfolio/static/js/2.8fdc4e18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/updated_portfolio/static/js/2.8fdc4e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22851a63ea7b960ae49",
+    "url": "/updated_portfolio/static/js/main.e486b27a.chunk.js"
+  },
+  {
+    "revision": "afcaf4c3e4af7fbbdd98",
+    "url": "/updated_portfolio/static/js/runtime-main.7470e116.js"
+  },
+  {
+    "revision": "ee6b19b364f59013cf5fd5a6e05604c0",
+    "url": "/updated_portfolio/static/media/Radnika-Medium.ee6b19b3.otf"
+  },
+  {
+    "revision": "2b0e0e0c76cf9786ba9195e059445b17",
+    "url": "/updated_portfolio/static/media/blue-wave.2b0e0e0c.svg"
+  },
+  {
+    "revision": "e354d281bdb7c19bbb729bfc42eda100",
+    "url": "/updated_portfolio/static/media/my-wave.e354d281.svg"
+  },
+  {
+    "revision": "48de8a43a9834d833e6889d8a5358e9b",
+    "url": "/updated_portfolio/static/media/red-orange.48de8a43.svg"
+  }
+]);
